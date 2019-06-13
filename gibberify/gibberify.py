fa267da0c01ab7948b41ec3198aac35b7b00e815
Lorenzo Gaifas <brisvag@gmail.com>
2019-06-13 17:33:49 +0200

    Added gui. Moved main entry point to __main__

--- gibberify/gibberify.py
+++ gibberify/gibberify.py
@@ -1,14 +1,12 @@
 """
-Main entry point of gibberify
+Where the translation actually happens
 """
 
-import json
 import re
 import pyphen
 import random
 import sys
 import os
-import argparse
 
 # local imports
 from gibberify.utils import __version__
@@ -137,46 +135,5 @@ def parse_message(somestring):
         return somestring
 
 
-def main():
-    # Parse arguments (also gives you help automatically with -h)
-    parser = argparse.ArgumentParser(prog='gibberify')
-    parser.add_argument('--from-lang', '-fl', dest='lang_in', type=str, default='en',
-                        choices=real_langs, help='language to translate from')
-    parser.add_argument('--to-lang', '-l', dest='lang_out', type=str, default='orc',
-                        choices=gib_langs.keys(), help='language to translate into')
-    parser.add_argument('--message', '-m', type=parse_message, nargs='*',
-                        help='text to translate. If a filename is given, the '
-                             'contents of the file will be translated to stdout. '
-                             'If `-` is given, input text is take from stdin')
-    args = parser.parse_args()
-
-    # if no arguments were given, run interactive mode
-    inter = False
-    if len(sys.argv) == 1:
-        inter = True
-
-    # Set some convenient variable names
-    if not inter:
-        lang_in = args.lang_in
-        lang_out = args.lang_out
-        text = ' '.join(args.message)
-
-    # fix path to files depending if we are running as script or as executable
-    if hasattr(sys, "_MEIPASS"):
-        data = os.path.join(sys._MEIPASS, 'data')
-    else:
-        data = os.path.join(os.path.dirname(__file__), 'data')
-
-    # load translation dictionaries
-    with open(os.path.join(data, 'dicts.json')) as f:
-        dicts = json.load(f)
-
-    if inter:
-        interactive(dicts)
-    else:
-        translator = dicts[lang_in][lang_out]
-        print(gibberify(translator, text))
-
-
 if __name__ == '__main__':
-    main()
+    raise NotImplementedError('this function is not implemented yet')
---
+++ gibberify/gui.py
@@ -0,0 +1,111 @@
+"""
+User interface using PyQt5
+"""
+
+import sys
+from PyQt5.QtGui import QFontDatabase
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QComboBox, QHBoxLayout,\
+    QVBoxLayout, QWidget
+
+# local imports
+from gibberify.config import real_langs, gib_langs
+from gibberify.gibberify import gibberify
+
+
+class LangMenu(QComboBox):
+    def __init__(self, lang_list):
+        super(LangMenu, self).__init__()
+        # use given language list as possible selections
+        self.addItems(lang_list)
+
+
+class TextBox(QTextEdit):
+    def __init__(self, placeholder, readonly=False):
+        super(TextBox, self).__init__()
+        # configure font
+        fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        fixedfont.setPointSize(12)
+        self.setFont(fixedfont)
+
+        # set default message
+        self.setPlaceholderText(placeholder)
+
+        # set readonly
+        self.setReadOnly(readonly)
+
+
+class MainWindow(QMainWindow):
+    """
+    main window class
+    """
+    def __init__(self, dicts):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle('Gibberify')
+        self.dicts = dicts
+
+        # create textboxes
+        self.text_in = TextBox('Type your text here.')
+        self.text_out = TextBox('Get your translation here.', readonly=True)
+
+        # create language menus
+        self.lang_in_box = LangMenu(real_langs)
+        self.lang_out_box = LangMenu(list(gib_langs.keys()))
+        # and initialize languages to current ones
+        self.lang_in = self.lang_in_box.currentText()
+        self.lang_out = self.lang_out_box.currentText()
+
+        # set up overall layout
+        # main container
+        lay_main = QHBoxLayout()
+        # two sides
+        lay_in = QVBoxLayout()
+        lay_out = QVBoxLayout()
+
+        # insert in/out sides in the main layout
+        lay_main.addLayout(lay_in)
+        lay_main.addLayout(lay_out)
+
+        # put main layout in container and set as central widget
+        container = QWidget()
+        container.setLayout(lay_main)
+        self.setCentralWidget(container)
+
+        # populate layout with our stuff
+        lay_in.addWidget(self.lang_in_box)
+        lay_in.addWidget(self.text_in)
+        lay_out.addWidget(self.lang_out_box)
+        lay_out.addWidget(self.text_out)
+
+        # update languages when combobox are modified
+        self.lang_in_box.currentTextChanged.connect(self.update_lang_in)
+        self.lang_out_box.currentTextChanged.connect(self.update_lang_out)
+
+        # run gibberify each time something changes
+        self.text_in.textChanged.connect(self.translate)
+        self.lang_in_box.currentTextChanged.connect(self.translate)
+        self.lang_out_box.currentTextChanged.connect(self.translate)
+        self.show()
+
+    def translate(self):
+        textin = self.text_in.toPlainText()
+        translator = self.dicts[self.lang_in][self.lang_out]
+        # set new text_out as translation
+        self.text_out.setText(gibberify(translator, textin))
+
+    def update_lang_in(self, value):
+        self.lang_in = value
+
+    def update_lang_out(self, value):
+        self.lang_out = value
+
+
+def gui(dicts):
+    app = QApplication(sys.argv)
+    app.setApplicationName('Gibberify')
+
+    window = MainWindow(dicts)
+    app.exec_()
+
+
+if __name__ == '__main__':
+    gui()
