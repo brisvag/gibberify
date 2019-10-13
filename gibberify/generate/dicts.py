1b45d9673b2b271a04a3b14785bea3409634c8fb
Lorenzo Gaifas <brisvag@gmail.com>
2019-10-13 15:34:13 +0200

    added run function, minor fixes and updated documentation

--- gibberify/generate/dicts.py
+++ gibberify/generate/dicts.py
@@ -1,16 +1,18 @@
+# Copyright 2019-2019 the gibberify authors. See copying.md for legal info.
+
 import random
 
 # local imports
 from .. import utils
 
 
-class Scrambler():
+class Scrambler:
     """
-    Scrambler class. Based on a given configuration, generates a straight and reverse dictionary
-    for the given combination of languages
-    of all the data needed to make custom dictionaries
-    """
+    Scrambler class
 
+    based on a given configuration, generates a straight and reverse
+    dictionary for the given combination of languages
+    """
     def __init__(self, real_lang, gib_lang, gib_conf):
         self.real_lang = real_lang
         self.gib_lang = gib_lang
@@ -33,14 +35,14 @@ class Scrambler():
 
         # get rid of part of the syllables NOT containing enriched patterns
         for pattern in self.gib_conf["enrich"]:
-            # get rid of 30% of syllables containing pattern
-            temp_pool = [syl for syl in pool_out if pattern in syl or random.choices([True, False], [0.7, 0.3])[0]]
+            # get rid of 50% of syllables containing pattern
+            temp_pool = [syl for syl in pool_out if pattern in syl or random.choices([True, False], [0.5, 0.5])[0]]
             pool_out = temp_pool
 
         # get rid of part of the syllables containing impoverished patterns
         for pattern in self.gib_conf["impoverish"]:
-            # get rid of 30% of syllables containing pattern
-            temp_pool = [syl for syl in pool_out if pattern not in syl or random.choices([True, False], [0.7, 0.3])[0]]
+            # get rid of 50% of syllables containing pattern
+            temp_pool = [syl for syl in pool_out if pattern not in syl or random.choices([True, False], [0.5, 0.5])[0]]
             pool_out = temp_pool
 
         # get rid of ALL the syllables containing forbidden patterns
@@ -49,7 +51,7 @@ class Scrambler():
             temp_pool = [syl for syl in pool_out if pattern not in syl]
             pool_out = temp_pool
 
-        self.gib_pool = pool_out
+        self.gib_pool = list(pool_out)
 
     def straight(self):
         """
@@ -58,12 +60,14 @@ class Scrambler():
 
         generates a dict that can be used to translate (almost) every syllable from the input language
         """
-        print(f'Creating translation dictionary for "{self.real_lang}-{self.gib_lang}"')
+        # TODO: see utils.pyphen about r_lang_codes
+        # print(f'Creating translation dictionary from {utils.r_lang_codes[self.real_lang]} to {self.gib_lang}...')
+        print(f'Creating translation dictionary from {self.real_lang} to {self.gib_lang}...')
         trans_dict = {}
 
+        # TODO: use shorter syllables more often, like in normal languages
         # load the required languages
         self.real_pool = utils.access_data('syllables', self.real_lang)
-
         # make sure we do enough times to map to all the input syllables (should not be a problem, but you never know)
         ratio = len(self.real_pool) // len(self.gib_pool)
         tmp_gib_pool = self.gib_pool
@@ -73,6 +77,7 @@ class Scrambler():
             final_gib_pool.extend(tmp_gib_pool)
 
         # do the actual mapping
+        # TODO: add some customization to mapping (length?...)
         for syl_real in self.real_pool:
             syl_gib = final_gib_pool.pop()
             trans_dict[syl_real] = syl_gib
@@ -83,12 +88,15 @@ class Scrambler():
         """
         takes an existing translation dictionary from real to gibberish and reverses it
         """
-        print(f'Creating translation dictionary for "{self.gib_lang}-{self.real_lang}"')
+        # TODO: see utils.pyphen about r_lang_codes
+        # print(f'Creating translation dictionary from {self.gib_lang} to {utils.r_lang_codes[self.real_lang]}...')
+        print(f'Creating translation dictionary from {self.gib_lang} to {self.real_lang}...')
         reverse = {}
 
         all_reverse = {k: v for v, k in self.dict_straight.items()}
 
         # sort by length, needed for reverse translation
+        # TODO: not sure this is really true. Need more testing.
         for k, v in all_reverse.items():
             ln = len(k)
             if ln not in reverse.keys():
@@ -103,3 +111,12 @@ class Scrambler():
         """
         utils.access_data('dicts', self.real_lang, self.gib_lang, write_data=self.dict_straight)
         utils.access_data('dicts', self.gib_lang, self.real_lang, write_data=self.dict_reverse)
+
+    def run(self):
+        """
+        main class method, runs all the other methods and writes to file
+        """
+        self.create_gib_pool()
+        self.straight()
+        self.reverse()
+        self.write()
