906f0d85c1d5e9e85700b775bfe95e8cd70a84f3
Lorenzo Gaifas <brisvag@gmail.com>
2019-10-13 15:39:41 +0200

    main entry point update to use new classes

--- gibberify/cli.py
+++ gibberify/cli.py
@@ -8,11 +8,12 @@ import os
 import sys
 import argparse
 
-# local imports. Here, they MUST actually be explicit, otherwise pyinstaller complains
-from gibberify import utils
-from gibberify import config
-from gibberify.generate import build
-from gibberify.translate import gibberify, direct_translator, gui
+# local imports
+from . import utils
+from .config import Config
+from .generate import build
+from .translate import Translator
+from .ui import gui, interactive
 
 
 def parse():
@@ -21,6 +22,7 @@ def parse():
 
     returns namespace containing named arguments
     """
+    # TODO: add option to delete all config and data files (useful for uninstall)
     # Parse arguments (also gives you help automatically with -h)
     parser = argparse.ArgumentParser(prog='gibberify', formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description='Gibberify is simple gibberish generator.\n'
@@ -62,74 +64,9 @@ def parse():
     return parser.parse_args()
 
 
-def interactive():
+def run(args):
     """
-    interactive mode. Deal with user input and call functions accordingly
-    """
-    conf = config.import_conf()
-    real_langs = conf['real_langs']
-    gib_langs = conf['gib_langs'].keys()
-
-    # TODO: add support for reverse translation
-    # Make it a sort of menu for easier usage
-    level = 0
-    while True:
-        try:
-            if level == 0:
-                # welcome and usage
-                print(f'Welcome to Gibberify {utils.version}! '
-                      f'Follow the prompts to translate a text.\n'
-                      f'To go back to the previous menu, press Ctrl+C.\n')
-                level += 1
-                continue
-
-            if level == 1:
-                lang_in = lang_out = ''
-
-                # language selection
-                while not lang_in:
-                    lang_in = input(f'What language do you want to translate from? '
-                                    f'Options are: {", ".join(real_langs)}.\n')
-                    # check if requested input language exists
-                    if lang_in not in real_langs:
-                        print(f'ERROR: you first need to generate a syllable pool for "{lang_in}"!')
-                        lang_in = ''
-                    else:
-                        lang_in = lang_in
-                        print(f'You chose "{lang_in}".')
-                while not lang_out:
-                    lang_out = input(f'What language do you want to translate into? '
-                                     f'Options are: {", ".join(gib_langs)}.\n')
-                    # check if requested output language exists
-                    if lang_out not in gib_langs:
-                        print(f'ERROR: you first need to generate a dictionary for "{lang_out}"!')
-                        lang_out = ''
-                    else:
-                        lang_out = lang_out
-                        print(f'You chose "{lang_out}".')
-                level += 1
-                continue
-
-            if level == 2:
-                translator = utils.access_data('dicts', lang_in, lang_out)
-                text = input('What do you want to translate?\n')
-                print(f'... or, as someone might say:\n'
-                      f'{gibberify(translator, text)}')
-                continue
-
-        except KeyboardInterrupt:
-            level -= 1
-            # exit the program if user tries to go back to level 0
-            if level < 1:
-                print('\nGood bye!\n')
-                return
-            print('\nGoing back...\n')
-            continue
-
-
-def dispatch(args):
-    """
-    takes namespace with named arguments and based on them dispatches to the right modules
+    takes namespace with named arguments and based on them control the main functions and modules of gibberify
     """
     if args.version:
         print(f'Gibberify {utils.version}')
@@ -138,35 +75,40 @@ def dispatch(args):
     # if no arguments were given, run gui version
     graphical = True if len(sys.argv) == 1 else False
 
-    conf = config.import_conf()
+    conf = Config.from_json()
 
     if args.config:
-        config.edit_conf()
+        conf.edit()
 
     if args.force_download or args.force_syllables or args.rebuild_dicts or args.config:
-        build(force_syllables=args.force_syllables, force_download=args.force_download)
+        build(conf, from_raw=args.force_download, force_syl_rebuild=args.force_syllables,
+              force_dicts_rebuild=args.rebuild_dicts)
         sys.exit()
 
     # before running anything, check if data files exist and create them if needed
-    for real_lang, gib_lang in zip(conf['real_langs'], conf['gib_langs']):
-        straight = utils.clean_path(utils.data, 'dicts', f'{real_lang}-{gib_lang}.json')
-        reverse = utils.clean_path(utils.data, 'dicts', f'{gib_lang}-{real_lang}.json')
-        if not any([os.path.isfile(straight), os.path.isfile(reverse)]):
-            print('Dictionaries are missing! I will generate all the data first. It may take a minute!\n')
-            build()
-            break
+    for real_lang in conf['real_langs']:
+        for gib_lang in conf['gib_langs'].keys():
+            straight = utils.data/'dicts'/f'{real_lang}-{gib_lang}.json'
+            reverse = utils.data/'dicts'/f'{gib_lang}-{real_lang}.json'
+            if not any([straight.is_file(), reverse.is_file()]):
+                print('Dictionaries are missing! I will generate the missing data first. It may take a minute!\n')
+                build(conf)
+                break
 
-    # dispatch
-    if graphical:
-        gui()
-    elif args.inter:
-        interactive()
+    if not graphical and not args.inter:
+        message = args.message[0]
+        print(Translator(args.lang_in, args.lang_out, message))
     else:
-        print(direct_translator(args.lang_in, args.lang_out, ' '.join(args.message)))
+        if graphical:
+            gui()
+        elif args.inter:
+            interactive()
+        else:
+            raise NotImplementedError('How did you get here?')
 
 
 def main():
     # get arguments
     args = parse()
     # dispatch to correct function based on arguments
-    dispatch(args)
+    run(args)
