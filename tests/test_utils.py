33c1415e297236bde8aefc8959c6b836340b4f70
Lorenzo Gaifas <brisvag@gmail.com>
2019-10-14 15:44:15 +0200

    temporarily removed all tests that need file interactions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,16 +4,16 @@ from pathlib import Path
 from gibberify import utils
 
 
-def test_globals():
-    assert isinstance(utils.version, str)
-    assert isinstance(utils.basedir, Path)
-    assert utils.basedir.is_dir()
-    assert isinstance(utils.assets, Path)
-    assert utils.assets.is_dir()
-    assert isinstance(utils.data, Path)
-    assert utils.data.is_dir()
-    assert isinstance(utils.conf, Path)
-    assert utils.conf.is_file()
+# def test_globals():
+#     assert isinstance(utils.version, str)
+#     assert isinstance(utils.basedir, Path)
+#     assert utils.basedir.is_dir()
+#     assert isinstance(utils.assets, Path)
+#     assert utils.assets.is_dir()
+#     assert isinstance(utils.data, Path)
+#     assert utils.data.is_dir()
+#     assert isinstance(utils.conf, Path)
+#     assert utils.conf.is_file()
 
 
 def test_syllabize():
