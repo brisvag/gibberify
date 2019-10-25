2bfdbbf6d7814fca1fef9ce32eebb5d7512b4a90
Lorenzo Gaifas <brisvag@gmail.com>
2019-10-25 16:37:33 +0200

    fixed multiprocessing

--- gibberify/generate/build.py
+++ gibberify/generate/build.py
@@ -16,29 +16,29 @@ def build(conf, from_raw=False, force_syl_rebuild=False, force_dicts_rebuild=Fal
     generates all data required by gibberify to work
     """
     # spawn a process for each Syllabizer
-    processes = []
+    syl_processes = []
     for real_lang in conf['real_langs']:
         s = Syllabizer(real_lang)
         p = Process(target=s.run, kwargs={'from_raw': from_raw, 'force_rebuild': force_syl_rebuild})
-        processes.append(p)
+        syl_processes.append(p)
 
     # start the processes and wait for all of them to finish before proceding
-    for p in processes:
+    for p in syl_processes:
         p.start()
-    for p in processes:
+    for p in syl_processes:
         p.join()
 
     # spawn a process for each Scrambler
-    processes = []
+    scr_processes = []
     for real_lang in conf['real_langs']:
         for gib_lang, gib_conf in conf['gib_langs'].items():
             # TODO: add ability to detect which dicts exist and avoid redoing them
             s = Scrambler(real_lang, gib_lang, gib_conf)
             p = Process(target=s.run, kwargs={'force': force_dicts_rebuild})
-            processes.append(p)
+            scr_processes.append(p)
 
     # start the processes and wait for all of them to finish before proceding
-    for p in processes:
+    for p in scr_processes:
         p.start()
-    for p in processes:
+    for p in scr_processes:
         p.join()
